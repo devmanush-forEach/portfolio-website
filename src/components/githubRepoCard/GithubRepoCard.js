d222f5ed956554e6c2be37efa1bf7d4cab2cc920
Nikhil <shakyanikhil12345@gmail.com>
2022-02-20 13:53:28 +0530

    updated project section

--- src/components/githubRepoCard/GithubRepoCard.js
+++ src/components/githubRepoCard/GithubRepoCard.js
@@ -12,44 +12,57 @@ export default function GithubRepoCard({ repo, theme }) {
   return (
     <div>
       <Fade bottom duration={2000} distance="40px">
-        <div
-          className="repo-card-div"
-          key={repo.id}
-          onClick={() => openRepoinNewTab(repo.url)}
+        <div className="project-detail-div"
           style={{ backgroundColor: theme.highlight }}
+          key={repo.id}
+        
         >
-          <div className="repo-name-div">
-            <svg
-              aria-hidden="true"
-              className="octicon repo-svg"
-              height="16"
-              role="img"
-              viewBox="0 0 12 16"
-              width="12"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"
-              ></path>
-            </svg>
-            <p className="repo-name" style={{ color: theme.text }}>
-              {repo.name}
-            </p>
+          <div className="project-img-div">
+            <a href = {repo.website_url} target = "_blank " > 
+            <img src={require(`../../assests/images/${repo.img_url}`)} alt="FrontendMasters" />
+            </a>
+
+
           </div>
-          <p className="repo-description" style={{ color: theme.text }}>
-            {repo.description}
-          </p>
-          <div className="repo-details">
-            <p
-              className="repo-creation-date subTitle"
-              style={{ color: theme.secondaryText }}
-            >
-              Created on {repo.createdAt.split("T")[0]}
+
+          <div
+            className="repo-card-div"
+            
+            onClick={() => openRepoinNewTab(repo.url)}
+          >
+            <div className="repo-name-div">
+              <svg
+                aria-hidden="true"
+                className="octicon repo-svg"
+                height="16"
+                role="img"
+                viewBox="0 0 12 16"
+                width="12"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"
+                ></path>
+              </svg>
+              <p className="repo-name" style={{ color: theme.text }}>
+                {repo.name}
+              </p>
+            </div>
+            <p className="repo-description" style={{ color: theme.text }}>
+              {repo.description}
             </p>
-            <ProjectLanguages
-              className="repo-languages"
-              logos={repo.languages}
-            />
+            <div className="repo-details">
+              <p
+                className="repo-creation-date subTitle"
+                style={{ color: theme.secondaryText }}
+              >
+                Created on {repo.createdAt.split("T")[0]}
+              </p>
+              <ProjectLanguages
+                className="repo-languages"
+                logos={repo.languages}
+              />
+            </div>
           </div>
           {/* <div className="repo-stats">
           <div className="repo-left-stat">
